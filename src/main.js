c0ed6bf2f55df0da5e74e76aa698cac44f04bd34
Marcel Schaeben <m.schaeben@uni-koeln.de>
2019-05-17 13:30:34 +0200

    clean up

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import './styles'
 
 import Vue from 'vue'
-// import VueMaterial from 'vue-material'
-// import 'vue-material/dist/vue-material.min.css'
-// import 'vue-material/dist/theme/default.css'
 import App from './components/App.vue'
 
 import Vuetify from 'vuetify'
