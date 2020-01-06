805993622277f2533bc97cfadd55027ae7767c45
Marcel Schaeben <m.schaeben@uni-koeln.de>
2020-01-06 22:00:51 +0100

    rename "Markers" in menu to "Layers"

--- src/components/MainPane.vue
+++ src/components/MainPane.vue
@@ -90,7 +90,7 @@
 
 							<v-list-group>
 								<v-list-tile slot="activator">
-									<v-list-tile-content><v-list-tile-title>Markers</v-list-tile-title></v-list-tile-content>
+									<v-list-tile-content><v-list-tile-title>Layers</v-list-tile-title></v-list-tile-content>
 								</v-list-tile>
 
 								<v-list-tile>
